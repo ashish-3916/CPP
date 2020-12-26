e8dcc2d611b5d99d5b346680261e2ce8e7620503
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2020-12-26 15:52:23 +0530

    Update Counting Bits.cpp

--- Leetcode/Counting Bits.cpp
+++ Leetcode/Counting Bits.cpp
@@ -1,25 +1,29 @@
-/*
-to count the no of set bits in  a #n we follow 
-
+/*Solution 1 :
+To count the # of set bits in a given n we follow :
+*/
+int cnt=0;
 while(n>0 )
 {
   cnt++;
   n = n & (n-1);
 }
+return cnt;
 
-now this can be written in a recursive manner 
+//now this can be written in a recursive manner :
 
-if(n==0)
-return 0;
+void f(n)
+{
+	if(n==0)
+		return 0;
+	return 1 + f(n&(n-1);
+}
+               /*
+The recurrence relation is :
+f(n)= 1 + f( n&(n-1) )
 
-return 1 + f(n&(n-1);
+which gives us the intuition of DP solution
 
-
-so f(n)= 1 + f(n&(n-1)
-
-which leads us to the solution to find in given range to do in O(N)
-
-*/
+which leads us to the solution to find # of set bits in given range to do in O(N)*/
 
   vector<int> countBits(int num) {
         vector<int>ans;
@@ -32,16 +36,26 @@ which leads us to the solution to find in given range to do in O(N)
         
         return ans;
     }
-    
-    
-    // other solution 
-    /* its to find for n/2 ie right shift of the no and check MSB of current no
-    */
-    
-     vector<int> countBits(int num) {
-        vector<int> v(num+1,0);
+               /*
+Solution 2:
+total # of set bits is n can be seen as set bits is n/2 ( right shift ) and checking LSB
+Example for n = 5
+1 0 1 <-- 5
+0 1 0 <-- (5/2=2 ie n>>1)
 
-        for(int i=1;i<= num; i++) v[i]=v[i/2]+i%2;
+so 2 contains 1 set bit and also we check for LSB of 5 , so 5 is odd , so 1 bit from there
+so total of 2 set bits in 5.
 
-        return v;
-    }
+relation can be written as
+
+f(n) = f(n/2) + n%2
+
+code:*/
+
+ vector<int> countBits(int num) {
+	vector<int> v(num+1,0);
+
+	for(int i=1;i<= num; i++) v[i]=v[i/2]+i%2;
+
+	return v;
+}
