a0f4722e536905b5e1fd717fcecfa77388bd6631
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-05-18 01:18:22 +0530

    Update Morris.cpp

--- Binary Trees/Traversals/Morris.cpp
+++ Binary Trees/Traversals/Morris.cpp
@@ -64,3 +64,69 @@ vector<int> preorderTraversal(TreeNode* root) {
         }
         return res ;
     }
+
+  vector<int> postorderTraversal(TreeNode* root)
+    {
+
+// Making our tree left subtree of a dummy TreeNode
+        TreeNode *dummyRoot = new TreeNode(0);
+        dummyRoot->left = root;
+        vector<int> res;
+//Think of P as the current TreeNode
+        TreeNode *p = dummyRoot, *pred, *first, *middle, *last;
+        while (p != NULL) {
+
+            if (p->left == NULL) {
+                p = p->right;
+            } else {
+                /* p has a left child => it also has a predeccessor
+                   make p as right child predeccessor of p
+                */
+                pred = p->left;
+                while (pred->right != NULL && pred->right != p) {
+                    pred = pred->right;
+                }
+
+                if (pred->right == NULL) {
+
+                    // predeccessor found for first time
+                    // modify the tree
+
+                    pred->right = p;
+                    p = p->left;
+
+                } else {
+
+                    // predeccessor found second time
+                    // reverse the right references in chain from pred to p
+                    first = p;
+                    middle = p->left;
+                    while (middle != p) {
+                        last = middle->right;
+                        middle->right = first;
+                        first = middle;
+                        middle = last;
+                    }
+
+                    // visit the nodes from pred to p
+                    // again reverse the right references from pred to p
+                    first = p;
+                    middle = pred;
+                    while (middle != p) {
+
+                        res.push_back(middle->val);
+                        last = middle->right;
+                        middle->right = first;
+                        first = middle;
+                        middle = last;
+                    }
+
+                    // remove the pred to TreeNode reference to restore the tree structure
+                    pred->right = NULL;
+                    p = p-> right;
+                }
+            }
+
+        }
+        return res;
+    }
