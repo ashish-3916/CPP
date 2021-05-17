b7b0a966e57624de00170443ed8ea1585d504c2e
Ashish Dangi <56604711+ashish-3916@users.noreply.github.com>
2021-05-18 00:55:08 +0530

    Update Morris.cpp

--- Binary Trees/Traversals/Morris.cpp
+++ Binary Trees/Traversals/Morris.cpp
@@ -30,3 +30,37 @@
     }
 
 
+vector<int> preorderTraversal(TreeNode* root) {
+        // node left right
+        TreeNode* curr = root; 
+        vector<int> res; 
+        
+        while(curr)
+        {
+            if(curr->left)
+            {
+                TreeNode* temp = curr->left;
+                while(temp->right !=NULL and temp-> right != curr ) // move to rightmost 
+                    temp=temp->right;
+                
+                if(temp->right==NULL)
+                {   temp->right = curr; // make a link 
+                    res.push_back(curr->val);
+                    curr=curr->left;
+                } 
+               
+                else if( temp-> right == curr)
+                {
+                    temp-> right= NULL; // remove the link 
+                    curr= curr-> right; // also curr is already visited , so we don't push it again
+                }
+                    
+            }
+            else
+            {
+                res.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        return res ;
+    }
